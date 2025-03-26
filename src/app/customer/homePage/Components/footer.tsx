ed77e2a91636c46ac0a3563e4527d68e2bd5656d
Baasandash0307 <bsskaa.kai123@gmail.com>
2025-03-26 16:36:18 +0800

    new

--- src/app/customer/homePage/Components/footer.tsx
+++ src/app/customer/homePage/Components/footer.tsx
@@ -1,54 +1,76 @@
-import React from 'react'
-
 export const Footer = () => {
-    return (
-        <div className='w-[1700px] h-[230px] grid grid-cols-5 '>
-            <div>
-                <div className="w-[46px] h-[37px]">
-                    <img src="/hand.png"></img>
-                </div>
-                <div className="text-[20px] flex">
-                    <p className="text-white">Nom</p>
-                    <p className="text-red-500">Nom</p>
-                </div>
-                <div>
-                    <p className="text-white text-[12px]">Swift delivery</p>
-                </div>
-            </div>
-
-            <div>
-                <p className='text-gray-600'>NOMNOM</p>
-                <button className='text-[16px] text-white'>Home</button>
-                <button className='text-[16px] text-white'>Contact us</button>
-                <button className='text-[16px] text-white'>Delivery zone</button>
-            </div>
-
-
-            <div>
-                <p className='text-gray-600'>MENU</p>
-                <button className='text-[16px] text-white'>Appetizer</button>
-                <button className='text-[16px] text-white'>Salads</button>
-                <button className='text-[16px] text-white'>Pizzas</button>
-                <button className='text-[16px] text-white'>Main dishes</button>
-                <button className='text-[16px] text-white'>Dessserts</button>
-            </div>
-            <div>
-                <p className='text-gray-600'>MENU</p>
-                <button className='text-[16px] text-white'>Side dish</button>
-                <button className='text-[16px] text-white'>Brunch</button>
-                <button className='text-[16px] text-white'>Desserts</button>
-                <button className='text-[16px] text-white'>Beverages</button>
-                <button className='text-[16px] text-white'>Fish & Sea foods</button>
-            </div>
-
-            <div className='flex'>
-                <p className='text-gray-600'>FOLLOW US</p>
-                <img className='w-[28px] h-[28px]' src='facebook.png'></img>
-                <img className='w-[28px] h-[28px]' src='instagram.png'></img>
-            </div>
-
+  return (
+    <div className="w-[1700px] h-[230px] grid grid-cols-5 mt-20">
+      <div>
+        <div className="w-[46px] h-[37px]">
+          <img src="/hand.png"></img>
         </div>
-    )
-}
+        <div className="text-[20px] flex">
+          <p className="text-white">Nom</p>
+          <p className="text-red-500">Nom</p>
+        </div>
+        <div>
+          <p className="text-white text-[12px]">Swift delivery</p>
+        </div>
+      </div>
+
+      <div>
+        <p className="text-gray-600">NOMNOM</p>
+        <div>
+          <button className="text-[16px] text-white">Home</button>
+        </div>
+        <div>
+          <button className="text-[16px] text-white">Contact us</button>
+        </div>
+        <div>
+          <button className="text-[16px] text-white">Delivery zone</button>
+        </div>
+      </div>
+
+      <div>
+        <p className="text-gray-600">MENU</p>
+        <div>
+          <button className="text-[16px] text-white">Appetizer</button>
+        </div>
+        <div>
+          <button className="text-[16px] text-white">Salads</button>
+        </div>
+        <div>
+          <button className="text-[16px] text-white">Pizzas</button>
+        </div>
+        <div>
+          <button className="text-[16px] text-white">Main dishes</button>
+        </div>
+        <div>
+          <button className="text-[16px] text-white">Dessserts</button>
+        </div>
+      </div>
+      <div>
+        <p className="text-gray-600">MENU</p>
+        <div>
+          <button className="text-[16px] text-white">Side dish</button>
+        </div>
+        <div>
+          <button className="text-[16px] text-white">Brunch</button>
+        </div>
+        <div>
+          <button className="text-[16px] text-white">Desserts</button>
+        </div>
+        <div>
+          <button className="text-[16px] text-white">Beverages</button>
+        </div>
+        <div>
+          <button className="text-[16px] text-white">Fish & Sea foods</button>
+        </div>
+      </div>
+
+      <div className="">
+        <p className="text-gray-600">FOLLOW US</p>
+        <img className="w-[28px] h-[28px]" src="facebook.png"></img>
+        <img className="w-[28px] h-[28px]" src="instagram.png"></img>
+      </div>
+    </div>
+  );
+};
 
 export default Footer;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,20 @@
-'use client'
+"use client";
 
 import { useState } from "react";
 import Header from "./customer/homePage/Components/header";
 import LoginPage from "./customer/loginPage/signup/components/page";
-import Categories from "./customer/homePage/Components/categories"
+import Categories from "./customer/homePage/Components/categories";
 import Card from "./customer/homePage/Components/card";
 import Footer from "./customer/homePage/Components/footer";
 
 export default function Home() {
-
   const [formValues, setFormValues] = useState({
     email: "",
     phoneNumber: "",
     password: "",
     ConfirmPassword: "",
     date: "",
-    image: ""
+    image: "",
   });
 
   return (
@@ -36,7 +35,7 @@ export default function Home() {
             </div>
           </div>
 
-          <div className="mt-10">
+          <div className="mt-10 bg-neutral-700 h-[500px]">
             <div className="flex justify-center">
               <div className="w-[1700px]">
                 <Card></Card>
@@ -45,10 +44,20 @@ export default function Home() {
           </div>
         </div>
 
-        <div className="w-full h-[755px] bg-black">
-          <Footer/>
-        </div>
+        <div className="w-full h-[450px] bg-black">
+          <div className="flex items-center ">
+            <div className="w-full h-[100px] overflow-hidden whitespace-nowrap bg-red-500 text-white py-2">
+              <div className="flex w-max animate-scroll">
+                <p className="text-4xl mx-4 font-bold">Fresh fast delivered</p>
+                <p className="text-4xl mx-4 font-bold">Fresh fast delivered</p>
+              </div>
+            </div>
+          </div>
 
+          <div className="flex justify-center">
+            <Footer />
+          </div>
+        </div>
       </div>
     </div>
   );
