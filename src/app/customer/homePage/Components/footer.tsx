9307e583d3af6a06405ab9d11d76e6c200bb6488
24LU4966 24LU4966 <bsskaa.kai123@gmail.com>
2025-03-26 11:37:39 +0800

    new

--- src/app/customer/homePage/Components/footer.tsx
+++ src/app/customer/homePage/Components/footer.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 export const Footer = () => {
     return (
         <div className='w-full h-[755px] bg-black'>
-            <div className="overflow-hidden whitespace-nowrap bg-red-500 text-white py-4 mt-10">
-                <div className="animate-marquee inline-block">
-                    <p className='text-[50px] font-bold gap-5 tracking-[15px]'>Fresh fast delivered    Fresh fast delivered     Fresh fast delivered     Fresh fast delivered Fresh fast delivered</p>
+            <div className='w-[1700px] h-[230px]'>
+                <div>
+                    <img src='hand.png' alt=''></img>
                 </div>
             </div>
         </div>
